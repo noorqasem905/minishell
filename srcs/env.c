c4f5b8e23e84509645db03e6fc7ed6c81d0f1721
amer962 <amer.alquraan1@outlook.com>
2025-04-19 20:10:39 +0300

    copy of evn

---
+++ srcs/env.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalquraa <aalquraa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/19 18:26:29 by aalquraa          #+#    #+#             */
+/*   Updated: 2025/04/19 19:11:07 by aalquraa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "../includes/minishell.h"
+
+char **my_env(char **env)
+{
+    int i;
+    char **my_env;
+
+    i = 0;
+    while (env[i])
+        i++;
+    my_env = malloc(sizeof(char *) * (i + 1));
+    if (!my_env)
+        return (NULL);
+    i = 0;
+    while (env[i])
+    {
+        my_env[i] = ft_strdup(env[i]);
+        if (!my_env[i])
+        {
+            while (--i >= 0)
+                free(my_env[i]);
+            free(my_env);
+            return (NULL);
+        }
+        i++;
+    }
+    my_env[i] = NULL;
+    return (my_env);
+}

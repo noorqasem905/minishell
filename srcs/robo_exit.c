3bcab7bc7a4f2a0e92a743551a2d6c27fa3a6c21
noor aldeen qasem <noorqasem905@gmail.com>
2025-05-18 18:42:21 +0300

    malloc free at robo exit

--- srcs/robo_exit.c
+++ srcs/robo_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   robo_exit.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aalquraa <aalquraa@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nqasem <nqasem@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 18:29:22 by aalquraa          #+#    #+#             */
-/*   Updated: 2025/05/14 19:21:24 by aalquraa         ###   ########.fr       */
+/*   Updated: 2025/05/18 18:38:56 by nqasem           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,9 +43,11 @@ void    robo_exit(char **str,t_cmd *cmd)
         {
             ft_printf("%2roboshell: exit: %s: numeric argument required", str[1]);
             cmd->exit_status = 255;
-            free(cmd->word);
-            free(cmd);
+            
+            free_list(&cmd->word);
+            free(cmd->here_doc);
             exit(cmd->exit_status);
+            free(cmd);
         }
         if (str[2])
         {
@@ -58,6 +60,7 @@ void    robo_exit(char **str,t_cmd *cmd)
     else
         n = cmd->exit_status;
         free_list(&cmd->word);
+        free(cmd->here_doc);
         free(cmd);
         exit((unsigned char)n);
 }
